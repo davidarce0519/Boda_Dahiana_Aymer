70ae139cea027291425a8b0f5dbf7d3a9a9d3905
davidarce0519 <david_alejandro.arce@uao.edu.co>
2026-09-11 14:51:01 -0500

    boda
    codigo completo

---
+++ Invitacion dahiana/front/invitados.js
@@ -0,0 +1,30 @@
+const invitadosData = [
+  {
+    "id": 1,
+    "grupo_id": "familia-mendoza",
+    "nombre": "María Alejandra Arce",
+    "integrantes": ["María Alejandra Arce", "Andrés Mendoza"],
+    "pases": 2
+  },
+  {
+    "id": 2,
+    "grupo_id": "familia-mendoza",
+    "nombre": "Andrés Mendoza",
+    "integrantes": ["María Alejandra Arce", "Andrés Mendoza"],
+    "pases": 2
+  },
+  {
+    "id": 3,
+    "grupo_id": "individual-dahiana",
+    "nombre": "Dahiana Gómez",
+    "integrantes": ["Dahiana Gómez"],
+    "pases": 1
+  },
+  {
+    "id": 4,
+    "grupo_id": "familia-burbano",
+    "nombre": "Hemer Burbano",
+    "integrantes": ["Hemer Burbano", "Acompañante"],
+    "pases": 2
+  }
+];
